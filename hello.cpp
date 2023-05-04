21cecf814c6d02d1dcc4f09d7ecd9b70f4124e0f
woc777 <61650870+woc777@users.noreply.github.com>
2023-05-04 18:16:34 +0800

    模拟冲突

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,6 @@
 int main() {
     std::cout << "演示修改项目提交并推送" << std::endl;
     std::cout << "再次修改项目" << std::endl;
-    std::cout << "第2题，分支操作" << std::endl;
+    std::cout << "第2题，分支操作" << std::endl; // 模拟冲突2
     return 0;
 }
